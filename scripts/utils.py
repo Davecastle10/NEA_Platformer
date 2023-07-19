a4fa0e134bab9054432c0ee674118d8df3d01f89
Dave Jones <davecastlejones10@gmail.com>
2023-07-19 22:13:30 +0100

    seems to be working again

--- scripts/utils.py
+++ scripts/utils.py
@@ -2,7 +2,7 @@ import pygame
 
 BASE_IMG_PATH = 'assets/images/'
 
-def load_image(path):
+def loadmage(path):
     img = pygame.image.load(BASE_IMG_PATH + path).convert()
     img.set_colorkey((0, 0, 0))
     return img
