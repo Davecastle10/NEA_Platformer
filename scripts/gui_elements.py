f030a4ab995bd639e99526b0229d1517d28bdcbf
Dave Jones <davecastlejones10@gmail.com>
2023-11-16 17:53:45 +0000

    added docstrings to the button constructor

--- scripts/gui_elements.py
+++ scripts/gui_elements.py
@@ -10,7 +10,16 @@ import pygame
 
 
 class Button:
+    """A class for buttons, that will most likely be used as a parent class for other GUI componets that I create in the future
+    """    
     def __init__(self, input_pos = (0,0), input_text = "", input_size = (100, 40), ):
+        """The constructor method for a Button object
+
+        Args:
+            input_pos (tuple of integers, optional): The (x,y) position of the top left hand corner of the Button. Defaults to (0,0).
+            input_text (str, optional): The text to be dispayed on the button -when I get round to adding this feature. Defaults to "".
+            input_size (tuple of integers, optional): The (x,y) size of the rect created for the button. Defaults to (100, 40).
+        """        
         self.text_to_display = input_text
         self.size = input_size
         self.pos = input_pos
