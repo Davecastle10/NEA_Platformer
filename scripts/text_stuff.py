65efb30f8c465214da3957fc67b391b81a4c9822
Dave Jones <davecastlejones10@gmail.com>
2023-11-24 14:15:31 +0000

    started making a seperate text class

---
+++ scripts/text_stuff.py
@@ -0,0 +1,30 @@
+import pygame
+
+class Text:
+    def __init__(self, font_name = 'Comic Sans'):# finish this later
+        self.font = pygame.font.Font(font_name)
+        pass
+
+    def display_text_simple(self, surf, text_pos, text_input):
+        """A simple procedure to display black text on a surface
+
+        Args:
+            surf (surface you want to display text on): the surface that you want to display text on
+            text_pos (tuple(int)): the x,y coords for the top left position of the text 
+            text_input (str): the strig you want to be displayed as text  
+        """    
+        display_text = self.font.render(text_input, False, (1, 1, 1))
+        surf.blit(display_text, text_pos)
+
+    
+def display_text_complicated(self,surf, text_pos, text_input, text_colour, bg_colour = None, wrap_length = 0):
+    """A more advanced procedure to display text of a chosen colour with a chosen background colour that can wrap lines to a surface
+
+    Args:
+        surf (surface you want to display text on): _description_
+        text_pos (tuple with the text location): _description_
+        text_input (str): the string of text you want to display 
+        text_colour (RGB code list/tuple thingy): The Rgb code .
+    """        
+    display_text = self.font.render(text_input, False, text_colour, bg_colour, wrap_length)
+    surf.blit(display_text, text_pos)
